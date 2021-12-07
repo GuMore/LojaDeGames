60aaa1a3fb1c4d0170d8739955216ef3e453cfbc
GuMore <gustavoglm1405@gmail.com>
2021-12-07 12:36:41 -0300

    Projeto de e-commerce loja de jogos executado junto ao grupo do projeto integrador

---
+++ minhaLojaDeGames/src/main/java/org/generation/minhalojadegames/repository/categoriaRepository.java
@@ -0,0 +1,13 @@
+package org.generation.minhalojadegames.repository;
+
+import java.util.List;
+
+import org.generation.minhalojadegames.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface categoriaRepository extends JpaRepository<Categoria, Long> {
+	public List<Categoria> findAllByCategoriaContainingIgnoreCase(String categoria);
+
+}
